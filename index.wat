b5ba53da477aacf1df9fa9b230c3e689cbf4a48b
Vince <vincent.hofer@students.unibe.ch>
2018-03-29 11:05:17 +0200

    Bugfix on variable definition starting by bracket

--- index.wat
+++ index.wat
@@ -1,8 +1,10 @@
 (module
 	(func $main (result f64)
 		(local $a f64)
-(set_local $a (f64.add (f64.const  1)(f64.mul (f64.const 2)(f64.const 3))))
-(f64.div (f64.mul (f64.sub (f64.add (f64.mul (get_local $a)(f64.const 2))(f64.const 1))(f64.const 14))(f64.const 6))(f64.add (f64.const 2)(f64.const 1)))
+(local $b f64)
+(set_local $a (f64.add (f64.const 1)(f64.mul (f64.const 2)(f64.const 3))))
+(set_local $b (f64.mul (f64.add (f64.mul (get_local $a)(f64.const 2))(f64.sub (f64.const 1)(f64.const 14)))(f64.const 6)))
+(f64.add (get_local $b)(f64.const 1))
 	)
 	(export "main" (func $main))
 )
--- parser.js
+++ parser.js
@@ -419,18 +419,24 @@ window.Parser =
     }
 
     function peg$parseAssign() {
-      var s0, s1, s2, s3;
+      var s0, s1, s2, s3, s4;
 
       s0 = peg$currPos;
       s1 = peg$parseSetVariable();
       if (s1 !== peg$FAILED) {
         s2 = peg$parseEqual();
         if (s2 !== peg$FAILED) {
-          s3 = peg$parseMathExpression();
+          s3 = peg$parse_();
           if (s3 !== peg$FAILED) {
-            peg$savedPos = s0;
-            s1 = peg$c4(s1, s2, s3);
-            s0 = s1;
+            s4 = peg$parseMathExpression();
+            if (s4 !== peg$FAILED) {
+              peg$savedPos = s0;
+              s1 = peg$c4(s1, s2, s4);
+              s0 = s1;
+            } else {
+              peg$currPos = s0;
+              s0 = peg$FAILED;
+            }
           } else {
             peg$currPos = s0;
             s0 = peg$FAILED;
