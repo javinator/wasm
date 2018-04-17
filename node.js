1f991d3f22020217a6bf791b092c1d7e0d782764
Vince <vincent.hofer@students.unibe.ch>
2018-04-17 21:13:21 +0200

    Some more work on AST (and added fibonacci example)

--- node.js
+++ node.js
@@ -1,52 +1,65 @@
-
-{ 
-	class Node {
+class Node {
 		constructor() {
-        		this.name = "Abstract Node";
-            		this.data = [];
+        	this.name = "Abstract Node";
+            this.data = [];
 			this.children = [];
 		}
 	}
     
-   	class Module extends Node {
-    		constructor(main,func) {
-        		super();
-            		this.name = "Module";
-            		this.children = [main,func];
+class Module extends Node {
+    	constructor(main,func) {
+        	super();
+            this.name = "Module";
+            this.children = [main,func];
 		}
-    	}
+    visit() {
+    	return visit.visitModule(this);
+    }
+    }
 
-	class DefineMain extends Node {
-    		constructor(exp) {
-        		super();
-            		this.name = "Define Main";
-            		this.children = [exp];
+class DefineMain extends Node {
+    	constructor(exp) {
+        	super();
+            this.name = "Define Main";
+            this.children = [exp];
 		}
+    visit() {
+    	return visit.visitDefineMain(this);
+    }
 	}
 
-	class DefineFunction extends Node {
-    		constructor(name,para,exp) {
-        		super();
-            		this.name = "Define Function";
-            		this.children = [exp];
-            		this.data = [name,para];
+class DefineFunction extends Node {
+    	constructor(name,para,exp) {
+        	super();
+            this.name = "Define Function";
+            this.children = [para,exp];
+            this.data = [name];
 		}
+    visit() {
+    	return visit.visitDefineFunction(this);
+    }
 	}
     
-    	class Expression extends Node {
-    		constructor(exp) {
-        		super();
-            		this.name = "Expression";
-            		this.children = [exp];
-        	}
+class Expression extends Node {
+    	constructor(exp) {
+        	super();
+            this.name = "Expression";
+            this.children = [exp];
+        }
+       visit() {
+    	return visit.visitExpression(this);
+    }
 	}
     
-    	class If extends Node {
-    		constructor(bool,exp) {
-        		super();
-            		this.name = "If";
-            		this.children = [bool,exp];
-        	}
+class If extends Node {
+    	constructor(bool,exp,elseexp) {
+        	super();
+            this.name = "If";
+            this.children = [bool,exp,elseexp];
+        }
+        visit() {
+    	return visit.visitIf(this);
+    }
 	}
     
     	class While extends Node {
@@ -55,6 +68,9 @@
             		this.name = "While";
             		this.children = [bool,exp];
         	}
+          visit() {
+    	return visit.visitWhile(this);
+    }
 	}
     
     	class Bool extends Node {
@@ -64,9 +80,11 @@
             		this.children = [head,tail];
             		this.data = [op];
         	}
+          visit() {
+    	return visit.visitBool(this);
+    }
 	}
 
-}
 
 	class CallFunction extends Node {
     		constructor(name,para) {
@@ -74,6 +92,9 @@
             		this.name = "Call Function";
             		this.data = [name,para];
         	}
+          visit() {
+    	return visit.visitCallFunction(this);
+    }
 	}
 
     	class CallParameter extends Node {
@@ -82,6 +103,9 @@
             		this.name = "Call Parameter";
             		this.children = [para];
         	}
+          visit() {
+    	return visit.visitCallParameter(this);
+    }
 	}
 
 	class Define extends Node {
@@ -90,23 +114,33 @@
             		this.name = "Define";
             		this.data = [char];
         	}
+          visit() {
+    	return visit.visitDefine(this);
+    }
 	}
 
 	class Assign extends Node {
     		constructor(char, exp) {
         		super();
             		this.name = "Assign";
-            		this.children = [char, exp];
+            		this.children = [exp];
+            		this.data = [char];
         	}
+          visit() {
+    	return visit.visitAssign(this);
+    }
 	}
 
 	class Math extends Node {
     		constructor(head,op,tail) {
         		super();
-            	this.name = "Math";
-            	this.children = [head,tail];
-            	this.data = [op];
+            		this.name = "Math";
+            		this.children = [head,tail];
+            		this.data = [op];
         	}
+          visit() {
+    	return visit.visitMath(this);
+    }
 	}
 
 	class Term extends Node {
@@ -116,6 +150,20 @@
             		this.children = [head,tail];
             		this.data = [op];
         	}
+          visit() {
+    	return visit.visitTerm(this);
+    }
+	}
+
+	class Parameter extends Node {
+    		constructor(char) {
+        		super();
+            		this.name = "Parameter";
+            		this.data = [char];
+        	}
+          visit() {
+    	return visit.visitParameter(this);
+    }
 	}
 
 	class Factor extends Node {
@@ -124,6 +172,9 @@
             		this.name = "Factor";
             		this.children = [exp];
         	}
+          visit() {
+    	return visit.visitFactor(this);
+    }
 	}
 
     	class Integer extends Node {
@@ -132,6 +183,9 @@
             		this.name = "Integer";
             		this.data = [int];
         	}
+          visit() {
+    	return visit.visitInteger(this);
+    }
 	}
     
     	class GetVariable extends Node {
@@ -140,6 +194,9 @@
             		this.name = "Get Variable";
             		this.data = [char];
         	}
+          visit() {
+    	return visit.visitGetVariable(this);
+    }
 	}
 
 	class Character extends Node {
@@ -148,12 +205,7 @@
             		this.name = "Character";
             		this.data = [char];
         	}
+          visit() {
+    	return visit.visitCharacter(this);
+    }
 	}
-}
-
-
-
-
-
-
-
